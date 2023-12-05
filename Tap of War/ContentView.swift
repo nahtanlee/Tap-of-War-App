9325c3ee1860700ac45abbe32540b4006ae01b96
Nathan Lee <nahtanlee@gmail.com>
2023-12-05 19:58:11 +1100

    Initial Commit

---
+++ Tap of War/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tap of War
+//
+//  Created by Michael Lee on 5/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tap of War/Tap_of_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tap_of_WarApp.swift
+//  Tap of War
+//
+//  Created by Michael Lee on 5/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tap_of_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
